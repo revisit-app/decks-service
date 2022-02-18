54b88c352a30b0c97ec064c9dc936551887badcd
zaid <iamzaidsheikh@gmail.com>
2022-02-18 18:08:20 +0530

    Set up db connection

---
+++ decks-service/src/main/java/io/github/revisit_app/decksservice/DecksServiceApplication.java
@@ -0,0 +1,33 @@
+package io.github.revisit_app.decksservice;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import io.github.revisit_app.decksservice.connection.DataStaxAstraProperties;
+
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+@SpringBootApplication
+public class DecksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecksServiceApplication.class, args);
+	}
+
+	@Bean
+	public CqlSession connectToAstra(DataStaxAstraProperties astraProperties) {
+
+		CqlSession cqlSession = CqlSession.builder()
+				.withCloudSecureConnectBundle(astraProperties.getSecureConnectBundle().toPath())
+				.withAuthCredentials(astraProperties.getUsername(),
+						astraProperties.getPassword())
+				.withKeyspace(astraProperties.getKeyspace())
+				.build();
+
+		return cqlSession;
+	}
+
+}
---
+++ decks-service/src/test/java/io/github/revisit_app/decksservice/DecksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.revisit_app.decksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
